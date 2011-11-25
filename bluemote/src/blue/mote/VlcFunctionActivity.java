21809e7080e9745969178d590bfbaf95a8f60e08
Kopierkatze <alexandra.weiss@mailbox.tu-dresden.de>
2011-11-25 18:57:07 +0100

    vlc buttons listening and sending. issue: time delay

--- bluemote/src/blue/mote/VlcFunctionActivity.java
+++ bluemote/src/blue/mote/VlcFunctionActivity.java
@@ -1,7 +1,19 @@
 package blue.mote;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class VlcFunctionActivity extends Activity {
 
@@ -9,10 +21,57 @@ public class VlcFunctionActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.vlc);
-		//bindKey(R.id.lauter);
+		bindKey(R.id.play, "space");
+		bindKey(R.id.louder, "keydown Control_L" + "\n" +
+				"key Up" + "\n" +
+				"keyup Control_L");
+		bindKey(R.id.quieter, "keydown Control_L" + "\n" +
+				"key Down" + "\n" +
+				"keyup Control_L");
+		bindKey(R.id.slowback, "minus");
+		bindKey(R.id.slowforward, "plus");
+		bindKey(R.id.fastback, "p");
+		bindKey(R.id.fastforward, "n");
 	}
 
-	private void bindKey(int lauter) {
+	void bindKey(final int btn_id, final String key) {
+		View btn = (View)findViewById(btn_id);
+		btn.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				sendKey(key);
+			}
+		});
+	}
+	
+	private void sendKey(String key){
+		URI ur = null;
+		try {
+			ur = new URI("http://192.168.2.106:4242/cmd");
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		StringEntity ent = null;
+		try {
+			ent = new StringEntity("key " + key + "\n");
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		}
+		ent.setContentEncoding("text/plain; charset=utf-8");
+		HttpPost post = new HttpPost();
+		post.setURI(ur);
+		post.setEntity(ent);
 		
+		HttpResponse response = null;
+		try {
+			long timestart = System.currentTimeMillis();
+			response = BluemoteActivity.httpclient.execute(post);
+			long timestop = System.currentTimeMillis();
+			System.out.println(timestop - timestart);
+			
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
